595977a86efa236fa070a065a5599a11d475518a
SarahOtmane <sarahotmane02@gmail.com>
2025-07-02 21:57:36 +0200

    Ajout des contrôleurs d'authentification pour l'enregistrement et la connexion des utilisateurs

---
+++ api/src/controllers/auth.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { registerUser, loginUser } from '../services/auth.service';
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { email, password, phone_number, name, role} = req.body;
+
+    await registerUser({ 
+        email, password, phone_number, name, role,
+    });
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error: any) {
+    res.status(400).json({ message: error.message || 'An error occurred' });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const token = await loginUser({ email, password });
+    res.status(200).json({ token });
+  } catch (error: any) {
+    res.status(400).json({ message: error.message || 'An error occurred' });
+  }
+};
