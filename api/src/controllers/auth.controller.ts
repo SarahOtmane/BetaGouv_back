78bcae37c5dab5f608c197f1b514d4b4e8dd7e61
SarahOtmane <sarahotmane02@gmail.com>
2025-07-02 22:16:12 +0200

    Ajout de la validation des champs requis lors de l'enregistrement d'un utilisateur

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -5,8 +5,12 @@ export const register = async (req: Request, res: Response) => {
   try {
     const { email, password, phone_number, name, role} = req.body;
 
+    if(!email || !password || !phone_number || !name || !role) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+
     await registerUser({ 
-        email, password, phone_number, name, role,
+      email, password, phone_number, name, role,
     });
 
     res.status(201).json({ message: 'User registered successfully' });
