d97ad064ede68c4dbdcf5f4574821a4f3dd11365
SarahOtmane <sarahotmane02@gmail.com>
2025-07-02 19:31:01 +0200

    Ajout du modèle de mise en avant mensuelle avec les champs id, photo, description, mouth, type et id_lycee

---
+++ api/src/models/mouthlyHighlightModel.ts
@@ -0,0 +1,37 @@
+import { DataTypes } from 'sequelize';
+import sequelize from "../config/database";
+
+const MouthlyHighlight = sequelize.define('MouthlyHighlight', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    photo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    mouth: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    id_lycee: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+}, {
+  tableName: 'mouthlyHighlights',
+  timestamps: true,
+  createdAt: true,
+  updatedAt: true
+});
+
+export default MouthlyHighlight;
