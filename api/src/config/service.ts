2587f2b943cc950b575187ada1444cb080237333
SarahOtmane <sarahotmane02@gmail.com>
2025-07-03 11:51:15 +0200

    Ajout de la configuration CORS et des routes pour la gestion des adresses et des écoles

--- api/src/config/service.ts
+++ api/src/config/service.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import { setupSwagger } from './swagger';
 
 import authRouter from '../routes/auth.route';
+import adressRouter from '../routes/adress.route';
+import schoolRouter from '../routes/school.route';
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 //middleware intégré à Express qui analyse les données encodées en URL 
     //comme les données de formulaire soumises via POST) et les expose dans req.body
@@ -13,7 +21,10 @@ app.use(express.urlencoded({ extended: true }));
     //et les expose dans req.body
 app.use(express.json());
 
-app.use('/auth', authRouter )
+app.use('/auth', authRouter );
+app.use('/adress', adressRouter);
+app.use('/school', schoolRouter);
+
 
 setupSwagger(app);
 
