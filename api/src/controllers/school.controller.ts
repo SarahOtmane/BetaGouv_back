783591dcbe276c2a466d85c771e914680da52cc6
SarahOtmane <sarahotmane02@gmail.com>
2025-07-03 11:51:07 +0200

    Ajout des contrôleurs pour la gestion des sites web des écoles

---
+++ api/src/controllers/school.controller.ts
@@ -0,0 +1,55 @@
+import { where } from "sequelize";
+import School from "../models/school.model";
+import User from "../models/user.model";
+
+
+export const addWebsiteToSchool = async (req, res) => {
+    const { website } = req.body;
+    const user = req.user; 
+
+    if (!user || user.role !== 'school') {
+        return res.status(403).json({ error: 'Forbidden: Only schools can add websites' });
+    }
+
+    if (!website) {
+        return res.status(400).json({ error: 'Website is required' });
+    }
+
+    try {
+        const user = await User.findByPk(req.user.id);
+        // Assuming School is a model imported from your models
+        await School.update(
+            { website: website },
+            { where: { id: user.getDataValue('id_role') } }
+        );
+        return res.status(201).json({ message: 'Website added successfully' });
+    } catch (error) {
+        console.error('Error adding website to school:', error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
+export const getSchool = async (req, res) => {
+    const user = req.user; 
+
+    if (!user || user.role !== 'school') {
+        return res.status(403).json({ error: 'Forbidden: Only schools can access this resource' });
+    }
+
+    try {
+        const user = await User.findByPk(req.user.id);
+
+        const school = await School.findOne({
+            where: { id: user.getDataValue('id_role') },
+        });
+
+        if (!school) {
+            return res.status(404).json({ error: 'School not found' });
+        }
+
+        return res.status(200).json(school);
+    } catch (error) {
+        console.error('Error fetching school:', error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+}
